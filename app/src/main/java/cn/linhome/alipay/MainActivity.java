de1a3b1648df764bff51619b455b9be0480d4eff
[ywb] <nathline1029@qq.com>
2019-02-20 15:05:14 +0800

    首次提交

--- app/src/main/java/cn/linhome/alipay/MainActivity.java
+++ app/src/main/java/cn/linhome/alipay/MainActivity.java
@@ -3,6 +3,9 @@ package cn.linhome.alipay;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import cn.linhome.library.pay.alipay.FAlipayer;
+import cn.linhome.library.pay.alipay.PayResult;
+
 public class MainActivity extends AppCompatActivity
 {
 
@@ -11,5 +14,21 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FAlipayer alipayer = new FAlipayer(this);
+        alipayer.setListener(new FAlipayer.FAlipayerListener()
+        {
+            @Override
+            public void onFailure(Exception e, String msg)
+            {
+
+            }
+
+            @Override
+            public void onResult(PayResult var1)
+            {
+
+            }
+        });
+        alipayer.pay("");
     }
 }
